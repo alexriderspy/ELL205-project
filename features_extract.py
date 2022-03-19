5400b035032d50dfb8aeb8303d4dc5d6f8328032
alexriderspy <sreemantidey1234@gmail.com>
2022-03-19 22:49:40 +0530

    part1 done

--- features_extract.py
+++ features_extract.py
@@ -1,6 +1,5 @@
 import cv2
 
-# To read image from disk, we use
-# cv2.imread function, in below method,
 img = cv2.imread("image.png", cv2.IMREAD_COLOR)
-print(img.shape)
+print(img.shape[0])
+print(img.shape[1])

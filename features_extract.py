2afce3d50da2559f7a82f0cc3bc95f79bb927b44
alexriderspy <sreemantidey1234@gmail.com>
2022-03-19 22:57:23 +0530

    part1

--- features_extract.py
+++ features_extract.py
@@ -3,3 +3,4 @@ import cv2
 img = cv2.imread("image.png", cv2.IMREAD_COLOR)
 print(img.shape[0])
 print(img.shape[1])
+print(img)
--- ssd.py
+++ ssd.py
@@ -9,4 +9,4 @@ def ssd(arr1,arr2):
             sumr = sumr + (arr1[i][j][0] - arr2[i][j][0])**2
             sumg = sumg + (arr1[i][j][1] - arr2[i][j][1])**2
             sumb = sumb + (arr1[i][j][2] - arr2[i][j][2])**2
-    return (sumr,sumg,sumb)
+    return sumr+sumg+sumb
